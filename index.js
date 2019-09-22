75f95c80f1df56e251523bc15588215dd775d0a6
Viktor <viktor.a.blizniuk@gmail.com>
2019-09-22 13:16:16 +0300

    CV

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-function partsSums(ls) {
-    let finalArray = [];
-    for (let i = 0; i < ls.length;) {
-        let sum = ls.reduce((p, c) => p + c, 0);
-        finalArray.push(sum);
-        ls.shift();
-    }
-    finalArray.push(0);
-    return finalArray;
-}
+alert ("hi")
